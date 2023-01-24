d72d67ab8c9c23c5bb8d6fe609cda37984ec5430
Cumbie <bcumbie@una.edu>
2023-01-24 16:41:02 -0600

    set up of ejs template engine

--- app.js
+++ app.js
@@ -2,17 +2,34 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000;  
 
-app.get('/', (req, res) => {
-  res.send('<!DOCTYPE html><html lang="en"><h1>Hello World, is it November?</h1>')
-})
+// set the view engine to ejs
 
+app.set('view engine', 'ejs');
+
+
+let myName = 'Barry'; 
+
+app.get('/', (req, res) => {
+//   res.send('<!DOCTYPE html><html lang="en"><h1>Hello World, is it November?</h1>')
+
+// res.send(myName);
+
+res.send(`<h3> Hi, ${ myName } </h3>`)
+
+})
 
 app.get('/show', (req, res) => {
 
     // res.sendFile('index.html');
-
     res.sendFile('index.html' , { root : __dirname});
+})
 
+app.get('/ejs', (req,res) => {
+
+    //ejs stuff goes here. 
+
+    // use res.render to load up an ejs view file
+    res.render('index');
 
 })
 
