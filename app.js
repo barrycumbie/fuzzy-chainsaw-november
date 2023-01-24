32c5b904bcb95915574df848fe8caa97af16ee62
Cumbie <bcumbie@una.edu>
2023-01-24 16:15:01 -0600

    created /show endpoint to send file

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const app = express()
 const PORT = process.env.PORT || 3000;  
 
 app.get('/', (req, res) => {
-  res.send('Hello World, is it November?')
+  res.send('<!DOCTYPE html><html lang="en"><h1>Hello World, is it November?</h1>')
+})
+
+
+app.get('/show', (req, res) => {
+
+    // res.sendFile('index.html');
+
+    res.sendFile('index.html' , { root : __dirname});
+
+
 })
 
 console.log('in the node console');
