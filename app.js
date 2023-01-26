a94f0788c8a215a832a67d3b20b860bfeb16c93f
Cumbie <bcumbie@una.edu>
2023-01-26 16:40:27 -0600

    got client to node data passed using form action and new endpoint

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ app.set('view engine', 'ejs');
 
 let myName = 'Barry'; 
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
 //   res.send('<!DOCTYPE html><html lang="en"><h1>Hello World, is it November?</h1>')
 
 // res.send(myName);
 
-res.send(`<h3> Hi, ${ myName } </h3>`)
+  let result = await res.send(`<h3> Hi, ${ myName } </h3>`);
+  
+  console.log( myName); 
+  
 
 })
 
@@ -24,14 +27,26 @@ app.get('/show', (req, res) => {
     res.sendFile('index.html' , { root : __dirname});
 })
 
+console.log("before app dot get to slash ejs", myName);
+
 app.get('/ejs', (req,res) => {
 
     //ejs stuff goes here. 
 
-    // use res.render to load up an ejs view file
-    res.render('index');
+    console.log("in /ejs before render:", myName);
 
+    // use res.render to load up an ejs view file
+    res.render('index', { myName: myName } ); //left one is ejs, right side is node land
+    console.log("after res render /ejs:", myName);
 })
+
+app.get('/name', (req,res) => {
+
+  console.log("in get to slash name:", req.query.ejsFormName); 
+  
+})
+
+console.log("after app dot get to slash ejs", myName);
 
 console.log('in the node console');
 
